124f41bcf2a877699f782423708adc508fdb4916
yuuuu280 <yuki280jp@icloud.com>
2023-12-27 10:08:48 +0900

    記事詳細のUI作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import ArticleNavigation from './components/ArticleNavigation';
+import ArticleDetail from './components/ArticleDetail';
 
 export default function Home() {
   return (
     <>
-      <ArticleNavigation />
+      <div className="flex max-w-screen-2xl mx-auto">
+        <ArticleNavigation />
+        <ArticleDetail />
+      </div>
     </>
   );
 }
