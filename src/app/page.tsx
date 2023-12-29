32bef9ab72b2a288663f40541d32fa8c06bc88be
hiroki3988 <hiroki.blank0329@gmail.com>
2023-12-29 17:23:43 +0900

    パスのidが未指定の時最初の記事にとばす

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
-import ArticleNavigation from './components/ArticleNavigation';
-import ArticleDetail from './components/ArticleDetail';
+"use client";
 
-export default function Home() {
-  return (
-    <>
-      <div className="flex max-w-screen-2xl mx-auto">
-        <ArticleNavigation />
-        <ArticleDetail />
-      </div>
-    </>
-  );
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default async function Home() {
+  type Article = {
+    id: number;
+  };
+
+  const router = useRouter();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/content/");
+        if (res.status !== 200) {
+          console.log("エラーが発生しました");
+          return;
+        }
+        const articleList = await res.json();
+        router.push(`/${articleList[0].id}`);
+      } catch (error) {
+        console.error("データの取得中にエラーが発生しました:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <></>;
 }
