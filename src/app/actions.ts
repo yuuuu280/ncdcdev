428b75debb2f2c5148b98594c5f0ecf85b4df4bd
hiroki3988 <hiroki.blank0329@gmail.com>
2023-12-30 14:11:24 +0900

    fetch部分の関数か

---
+++ src/app/actions.ts
@@ -0,0 +1,15 @@
+type Article = {
+  id: number;
+  title: string;
+};
+
+export const getContents = async (): Promise<Article[]> => {
+  "use server";
+  const res = await fetch("http://localhost:3000/content/");
+  if (res.status !== 205) {
+    console.log("エラーが発生しました");
+    throw new Error("エラーが発生しました");
+  }
+  const articleList = await res.json();
+  return articleList;
+};
