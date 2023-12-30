4430758cf4303e67628207279fdc1d2f5607f9e9
hiroki3988 <hiroki.blank0329@gmail.com>
2023-12-30 10:13:18 +0900

    エラー解消

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,13 @@
 import ArticleSideber from "../components/ArticleSidebar";
 import ArticleDetail from "../components/ArticleDetail";
 
-export default function Home({ params }: { params: { id: string } }) {
+export default async function Home({ params }: { params: { id: string } }) {
   return (
     <>
       <div className="flex max-w-screen-2xl mx-auto">
+        {/* @ts-expect-error Server Component */}
         <ArticleSideber id={params.id} />
+        {/* @ts-expect-error Server Component */}
         <ArticleDetail id={params.id} />
       </div>
     </>
